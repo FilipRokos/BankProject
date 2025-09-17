e5d74bac4ba6b88bdd64d72d9c9613eec6e38de0
FilipRokos <168109262+FilipRokos@users.noreply.github.com>
2025-09-17 19:16:37 +0200

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,19 @@
+
 package org.example;
+
+import org.example.Accounts.BaseAccount;
+import org.example.Accounts.StudentBankAccount;
+import org.example.people.BaseHuman;
+import org.example.people.Student;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BaseAccount studentskyAccount = new StudentBankAccount("454545454",1000,"454545454", (new Student("5454545","Matyáš", "Murphy", "Delta")));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if(studentskyAccount.GetOwner() instanceof Student) {
+            System.out.println(((Student) studentskyAccount.GetOwner()).GetSchool());
         }
     }
 }
